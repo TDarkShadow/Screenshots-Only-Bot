3aa79d7238ad08c4790ff40822d9a2f1ea18a47b
TDarkShadow <5389859+TDarkShadow@users.noreply.github.com>
2019-12-05 01:32:14 +0100

    :sparkles: Discord Bot template applied.

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", message => {
+  if (message.author.bot) return;
+});
+
+client.login();
